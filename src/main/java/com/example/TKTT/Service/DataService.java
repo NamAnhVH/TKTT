a7aa65284a7b9d1f42ea9630ab3620b9d87922ca
NamAnhVH <namanh12022002@gmail.com>
2023-12-08 14:29:57 +0700

    update

--- src/main/java/com/example/TKTT/Service/DataService.java
+++ src/main/java/com/example/TKTT/Service/DataService.java
@@ -12,6 +12,9 @@ import java.util.List;
 @Service
 public class DataService {
 
+    private static final String CUT_FIRST_STRING = "Read\n";
+    private static final String CUT_LAST_STRING = "Similar Reads\n";
+
     public List<Doc> searchData(String query) {
         String apiUrl = "http://localhost:8983/solr/nutch/select?df=content&fl=url,content,title&indent=true&q.op=OR&q="
                 + query +
@@ -24,8 +27,24 @@ public class DataService {
         if (responseEntity.getStatusCode().is2xxSuccessful()) {
             Data responseData = responseEntity.getBody();
             assert responseData != null;
-            return responseData.getResponse().getDocs();
+            return filterContent(responseData.getResponse().getDocs());
         }
         return new ArrayList<>();
+    }
+
+    private List<Doc> filterContent(List<Doc> docs) {
+        List<Doc> updateContentDocs = new ArrayList<>();
+        for (Doc doc: docs){
+            List<String> updateContents = new ArrayList<>();
+            for(String content: doc.getContent()){
+                int indexFirst = content.indexOf(CUT_FIRST_STRING);
+                int indexLast = content.indexOf(CUT_LAST_STRING);
+                String updateContent = (indexFirst != -1 && indexLast != -1) ? content.substring(indexFirst + CUT_FIRST_STRING.length(), indexLast) : content;
+                updateContents.add(updateContent);
+            }
+            doc.setContent(updateContents);
+            updateContentDocs.add(doc);
+        }
+        return updateContentDocs;
     }
 }
